6de985ab144b6d42254398209efa627884b99536
daniellinslopes <daniboy237@proton.me>
2026-03-20 17:14:25 -0300

    poo-banco-digital

--- br/com/bancodigital/model/Cliente.java
+++ br/com/bancodigital/model/Cliente.java
@@ -3,7 +3,7 @@ package model;
 public record Cliente(String nome) {
     public Cliente {
         if (nome == null || nome.isBlank()) {
-            throw new IllegalArgumentException("O nome. do cliente nao pode ser vazio.");
+            throw new IllegalArgumentException("O nome do cliente nao pode ser vazio.");
         }
     }
 }
