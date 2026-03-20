cb4f582bf8a5c506cc67f26ab2c76bb46f56750e
daniellinslopes <daniboy237@proton.me>
2026-03-20 17:22:49 -0300

    poo-banco-digital

--- br/com/bancodigital/model/ContaPoupanca.java
+++ br/com/bancodigital/model/ContaPoupanca.java
@@ -1,5 +1,30 @@
 package model;
 
-public class ContaPoupanca {
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
+public class ContaPoupanca extends Conta {
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+
+    public void aplicarRendimento(BigDecimal taxaPercentual) {
+        validarValor(taxaPercentual);
+
+        BigDecimal rendimento = saldo
+                .multiply(taxaPercentual)
+                .divide(new BigDecimal("100"), 2, RoundingMode.HALF_EVEN);
+
+        saldo = saldo.add(rendimento);
+        registrarTransacao(TipoTransacao.RENDIMENTO, rendimento, "Rendimento aplicado de " + taxaPercentual + "%"
+
+        );
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("\n=== EXTRATO CONTA POUPANÇA ===");
+        imprimirInfosComuns();
+        imprimirHistorico();
+    }
 }
