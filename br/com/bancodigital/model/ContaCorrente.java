94e412f6e1653e254e767d3c5f21643751e995d2
daniellinslopes <daniboy237@proton.me>
2026-03-20 17:13:08 -0300

    poo-banco-digital

--- br/com/bancodigital/model/ContaCorrente.java
+++ br/com/bancodigital/model/ContaCorrente.java
@@ -1,5 +1,31 @@
 package model;
 
-public class ContaCorrente {
+import java.math.BigDecimal;
 
+public class ContaCorrente extends Conta {
+    private static final BigDecimal TAXA_SAQUE = new BigDecimal("1.50");
+
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void sacar(BigDecimal valor) {
+        validarValor(valor);
+        BigDecimal total = valor.add(TAXA_SAQUE);
+        validarSaldo(total);
+
+        saldo = saldo.subtract(valor);
+        saldo = saldo.subtract(TAXA_SAQUE);
+
+        registrarTransacao(TipoTransacao.SAQUE, valor, "Saque em conta corrente");
+        registrarTransacao(TipoTransacao.TAXA, TAXA_SAQUE, "Taxa de saque");
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("\n=== EXTRATO CONTA CORRENTE ===");
+        imprimirInfosComuns();
+        imprimirHistorico();
+    }
 }
